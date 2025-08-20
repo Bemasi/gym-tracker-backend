4a64b3d7857d5fa0fa1a2474560e1206a3e1324c
Benjamín Mato <71629090+Bemasi@users.noreply.github.com>
2025-08-20 18:14:30 +0200

    Erase contructor in DTO
    No word to be added :(

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -2,6 +2,7 @@ import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsEmail, IsOptional, MinLength } from 'class-validator';
 
 export class UpdateUserDto {
+
   @ApiPropertyOptional()
   @IsOptional()
   name?: string;
